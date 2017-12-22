51eb5e88346efb665b69aecf7877a4c0c99160e7
Bill Finn <bill@finndigital.com>
2017-12-22 15:00:22 -0600

    update the scope

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -9,7 +9,7 @@ class Tag < ApplicationRecord
   scope :product_brand, ->{where(ancestry: 5)}
   scope :audience, ->{where(ancestry: 2)}
   scope :industry, ->{where(ancestry: 7)}
-  scope :installation, ->{where(name: 'Installation')}
-  scope :region, ->{where(name: 'Region')}
-  scope :lead_type, ->{where(name: 'Lead Type')}
+  scope :installation, ->{where(ancestry: 8)}
+  scope :region, ->{where(ancestry: 12)}
+  scope :lead_type, ->{where(ancestry: 11)}
 end
