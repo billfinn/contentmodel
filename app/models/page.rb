a0d96732feb733eac1d2a0f6b6baba133d7413a2
Bill Finn <bill@finndigital.com>
2018-02-07 18:51:45 -0600

    connect page detail to sitemap link

--- app/models/page.rb
+++ app/models/page.rb
@@ -6,26 +6,12 @@ class Page < ApplicationRecord
   has_many :tags, :through => :tag_mappings
   accepts_nested_attributes_for :tag_mappings, :allow_destroy => true
 
-  has_many :hierarchy_mappings
-  has_many :hierarchies, :through => :hierarchy_mappings
-  accepts_nested_attributes_for :hierarchy_mappings, :allow_destroy => true
-
   has_many :seo_mappings
   has_many :seo_terms, :through => :seo_mappings
   accepts_nested_attributes_for :seo_mappings, :allow_destroy => true
 
-  has_many :site_page_mappings
-  has_many :sites, :through => :site_page_mappings
-  accepts_nested_attributes_for :site_page_mappings, :allow_destroy => true
-
   belongs_to :site
-  # belongs_to :page_workflow
-  # belongs_to :workflow
 
   validates :name, :presence => true
-
-  # has_attached_file :image
-  # validates_attachment_file_name :image, :matches => [/_\d+_\d+_\d+\.bin$/]
-  # validates_uniqueness_of :image_file_name # this is a standard ActiveRecord validator
 
 end
