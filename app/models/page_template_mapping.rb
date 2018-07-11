f46d39921befc141dcebfec6a0e5783f33055996
Bill Finn <bill@finndigital.com>
2018-07-11 08:25:55 -0500

    groom template component display

--- app/models/page_template_mapping.rb
+++ app/models/page_template_mapping.rb
@@ -1,4 +1,9 @@
 class PageTemplateMapping < ApplicationRecord
   belongs_to :page
   belongs_to :template
+
+  accepts_nested_attributes_for :page
+  accepts_nested_attributes_for :template
+
+
 end
